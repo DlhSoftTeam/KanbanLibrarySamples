1471dfbb8365fd53c5b52e426c5e2e1fde460eba
DlhSoft <Contact@DlhSoft.com>
2021-10-28 18:24:41 +0300

    Name fixes

--- KanbanBoardCustomAppearanceSample/App.xaml.cs
+++ KanbanBoardCustomAppearanceSample/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace KanbanBoardCustomAppearance
+namespace KanbanBoardCustomAppearanceSample
 {
     /// <summary>
     /// Interaction logic for App.xaml
