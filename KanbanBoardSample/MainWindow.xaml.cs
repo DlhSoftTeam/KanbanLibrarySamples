874aba44eeb66518e31d5158986193ac48cefedb
DlhSoft <Contact@DlhSoft.com>
2022-01-12 18:56:41 +0200

    +

--- KanbanBoardSample/MainWindow.xaml.cs
+++ KanbanBoardSample/MainWindow.xaml.cs
@@ -32,47 +32,56 @@ namespace KanbanBoardSample
             var state4 = new KanbanState { Content = "In testing", AreNewItemButtonsHidden = true };
             var state5 = new KanbanState { Content = "Done", IsCollapsedByDefaultForGroups = true, AreNewItemButtonsHidden = true };
             var states = new ObservableCollection<KanbanState> { state1, state2, state3, state4, state5 };
+            var groupStates = new ObservableCollection<KanbanState> { state1, state2, state5 };
 
-            var resource1 = new KanbanResource { Content = "Clarissa Candelaria", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Resource1.png", UriKind.RelativeOrAbsolute)) };
-            var resource2 = new KanbanResource { Content = "Tyson Lamberson", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Resource2.png", UriKind.RelativeOrAbsolute)) };
-            var resource3 = new KanbanResource { Content = "Joanna Mcamis", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Resource2.png", UriKind.RelativeOrAbsolute)) };
-            var resource4 = new KanbanResource { Content = "Jed Markovitz", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Resource2.png", UriKind.RelativeOrAbsolute)) };
+            var resource1 = new KanbanResource { Content = "Clarissa Candelaria", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Clarissa.png", UriKind.RelativeOrAbsolute)) };
+            var resource2 = new KanbanResource { Content = "Tyson Lamberson", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Tyson.png", UriKind.RelativeOrAbsolute)) };
+            var resource3 = new KanbanResource { Content = "Joanna Mcamis", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Joanna.png", UriKind.RelativeOrAbsolute)) };
+            var resource4 = new KanbanResource { Content = "Jed Markovitz", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Jed.png", UriKind.RelativeOrAbsolute)) };
             var resources = new ObservableCollection<KanbanResource> { resource1, resource2, resource3, resource4 };
 
-            var group1 = new KanbanGroup { Content = "Planning", State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource1 } };
-            var group2 = new KanbanGroup { Content = "Implementation", State = state3, AssignedResources = new ObservableCollection<KanbanResource> { resource2 } };
-            var group3 = new KanbanGroup { Content = "Marketing", State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource1 } };
-            var groups = new ObservableCollection<KanbanGroup> { group1, group2, group3 };
+            var group1 = new KanbanGroup { Content = "Development", State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource1, resource2 } };
+            var group2 = new KanbanGroup { Content = "Marketing", State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource3 } };
+            var groups = new ObservableCollection<KanbanGroup> { group1, group2 };
 
             var items = new ObservableCollection<KanbanItem>
             {
-                new KanbanItem { Content = "Task 1", Group = group1, State = state1, AssignedResources = new ObservableCollection<KanbanResource> { resource1 } },
-                new KanbanItem { Content = "Task 2", Group = group1, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource1 } },
-                new KanbanItem { Content = "Bug 1", Group = group1, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource1 }, ItemType = KanbanBoard.BugItemType },
-                new KanbanItem { Content = "Task 3", Group = group1, State = state1, AssignedResources = new ObservableCollection<KanbanResource> { resource2 } },
-                new KanbanItem { Content = "Task 4", Group = group1, State = state1, AssignedResources = new ObservableCollection<KanbanResource> { resource1 } },
-                new KanbanItem { Content = "Task 5", Group = group2, State = state1, AssignedResources = new ObservableCollection<KanbanResource> { resource2 } },
-                new KanbanItem { Content = "Task 6", Group = group2, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource2 } },
-                new KanbanItem { Content = "Task 7", Group = group2, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource1 } },
-                new KanbanItem { Content = "Task 8", Group = group2, State = state3, AssignedResources = new ObservableCollection<KanbanResource> { resource2 } },
+                new KanbanItem { Content = "Architecture", Group = group1, State = state5, AssignedResources = new ObservableCollection<KanbanResource> { resource2 } },
+                new KanbanItem { Content = "Date-times", Group = group1, State = state4, AssignedResources = new ObservableCollection<KanbanResource> { resource3 } },
+                new KanbanItem { Content = "Schedules", Group = group1, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource3, resource4 } },
+                new KanbanItem { Content = "Weekend issue", Group = group1, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource4 }, ItemType = KanbanBoard.BugItemType },
+                new KanbanItem { Content = "Diagram", Group = group1, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource1 } },
+                new KanbanItem { Content = "Bars", Group = group1, State = state1, AssignedResources = new ObservableCollection<KanbanResource> { resource1 } },
+                new KanbanItem { Content = "Nonworking time highlighting", Group = group1, State = state3, AssignedResources = new ObservableCollection<KanbanResource> { resource4 } },
+                new KanbanItem { Content = "Background", Group = group1, State = state3 },
+                new KanbanItem { Content = "Milestone bug", Group = group1, State = state3, AssignedResources = new ObservableCollection<KanbanResource> { resource1 }, ItemType = KanbanBoard.BugItemType },
+                new KanbanItem { Content = "Drag and drop", Group = group1, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource2 } },
+                new KanbanItem { Content = "Preparations", Group = group2, State = state5, AssignedResources = new ObservableCollection<KanbanResource> { resource1 } },
+                new KanbanItem { Content = "Colors", Group = group2, State = state5, AssignedResources = new ObservableCollection<KanbanResource> { resource3 } },
+                new KanbanItem { Content = "Logo", Group = group2, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource3 } },
+                new KanbanItem { Content = "Sample apps", Group = group2, State = state3, AssignedResources = new ObservableCollection<KanbanResource> { resource4 } },
+                new KanbanItem { Content = "Screenshots", Group = group2, State = state2, AssignedResources = new ObservableCollection<KanbanResource> { resource4 } },
+                new KanbanItem { Content = "Web site", Group = group2, State = state1, AssignedResources = new ObservableCollection<KanbanResource> { resource2, resource4 } },
+                new KanbanItem { Content = "Social networking", Group = group2, State = state1, AssignedResources = new ObservableCollection<KanbanResource> { resource1, resource3 } },
             };
 
             // Uncomment the following code lines to load more groups and items.
-            //for (var i = 3; i <= 10; i++)
-            //{
-            //    var group = new KanbanGroup { Content = "Story " + i, AssignedResource = resource1 };
-            //    groups.Add(group);
-            //    for (var j = 1; j <= 10; j++)
-            //    {
-            //        var item = new KanbanItem { Content = "Item " + i + "." + j, Group = group, State = j % 2 == 0 ? state1 : state2, AssignedResource = resource1 };
-            //        items.Add(item);
-            //    }
-            //}
+            // for (var i = 3; i <= 10; i++)
+            // {
+            //     var group = new KanbanGroup { Content = "Story " + i, AssignedResource = resource1 };
+            //     groups.Add(group);
+            //     for (var j = 1; j <= 10; j++)
+            //     {
+            //         var item = new KanbanItem { Content = "Item " + i + "." + j, Group = group, State = j % 2 == 0 ? state1 : state2, AssignedResource = resource1 };
+            //         items.Add(item);
+            //     }
+            // }
 
             KanbanBoard.States = states;
             KanbanBoard.Groups = groups;
             KanbanBoard.Items = items;
             KanbanBoard.AssignableResources = resources;
+            KanbanBoard.GroupStates = groupStates;
 
             KanbanBoard.NewItemAdded += (sender, e) =>
             {
