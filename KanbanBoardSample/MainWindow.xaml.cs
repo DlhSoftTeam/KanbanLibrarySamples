4e81a4dc9f8a23940a25791802172c6a28d85643
DlhSoft <Contact@DlhSoft.com>
2022-01-28 17:06:37 +0200

    +

--- KanbanBoardSample/MainWindow.xaml.cs
+++ KanbanBoardSample/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace KanbanBoardSample
@@ -27,6 +28,10 @@ namespace KanbanBoardSample
             var states = new ObservableCollection<KanbanState> { state1, state2, state3, state4, state5 };
             var groupStates = new ObservableCollection<KanbanState> { state1, state2, state5 };
 
+            var category1 = new KanbanCategory { Content = "Questions", Background = Brushes.CornflowerBlue, BorderBrush = Brushes.Blue, Foreground = Brushes.White };
+            var category2 = new KanbanCategory { Content = "Important", Background = Brushes.LightGreen, BorderBrush = Brushes.Green };
+            var categories = new ObservableCollection<KanbanCategory> { category1, category2 };
+
             var resource1 = new KanbanResource { Content = "Clarissa Candelaria", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Clarissa.png", UriKind.RelativeOrAbsolute)) };
             var resource2 = new KanbanResource { Content = "Tyson Lamberson", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Tyson.png", UriKind.RelativeOrAbsolute)) };
             var resource3 = new KanbanResource { Content = "Joanna Mcamis", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Joanna.png", UriKind.RelativeOrAbsolute)) };
@@ -39,20 +44,20 @@ namespace KanbanBoardSample
             
             var items = new ObservableCollection<KanbanItem>
             {
-                new KanbanItem { Content = "Architecture", Group = group1, State = state5, Resources = new ObservableCollection<KanbanResource> { resource2 }, ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Architecture.png", UriKind.RelativeOrAbsolute)) },
+                new KanbanItem { Content = "Architecture", Group = group1, State = state5, Resources = new ObservableCollection<KanbanResource> { resource2 }, ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Architecture.png", UriKind.RelativeOrAbsolute)), Categories = new ObservableCollection<KanbanCategory> { category2 } },
                 new KanbanItem { Content = "Date-times", Group = group1, State = state4, Resources = new ObservableCollection<KanbanResource> { resource3 }, ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/DateTimes.png", UriKind.RelativeOrAbsolute)) },
-                new KanbanItem { Content = "Schedules", Group = group1, State = state2, Resources = new ObservableCollection<KanbanResource> { resource3, resource4 } },
+                new KanbanItem { Content = "Schedules", Group = group1, State = state2, Resources = new ObservableCollection<KanbanResource> { resource3, resource4 }, Categories = new ObservableCollection<KanbanCategory> { category1, category2 } },
                 new KanbanItem { Content = "Weekend issue", Group = group1, State = state2, Resources = new ObservableCollection<KanbanResource> { resource4 }, ItemType = KanbanBoard.BugItemType, ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/Bug.png", UriKind.RelativeOrAbsolute)) },
-                new KanbanItem { Content = "Diagram", Group = group1, State = state2, Resources = new ObservableCollection<KanbanResource> { resource1 } },
-                new KanbanItem { Content = "Bars", Group = group1, State = state1, Resources = new ObservableCollection<KanbanResource> { resource1 } },
+                new KanbanItem { Content = "Diagram", Group = group1, State = state2, Resources = new ObservableCollection<KanbanResource> { resource1 }, Categories = new ObservableCollection<KanbanCategory> { category2 } },
+                new KanbanItem { Content = "Bars", Group = group1, State = state1, Resources = new ObservableCollection<KanbanResource> { resource1 }, Categories = new ObservableCollection<KanbanCategory> { category2 } },
                 new KanbanItem { Content = "Nonworking time highlighting", Group = group1, State = state3, Resources = new ObservableCollection<KanbanResource> { resource4 } },
                 new KanbanItem { Content = "Background", Group = group1, State = state3 },
                 new KanbanItem { Content = "Milestone bug", Group = group1, State = state3, Resources = new ObservableCollection<KanbanResource> { resource1 }, ItemType = KanbanBoard.BugItemType },
-                new KanbanItem { Content = "Drag and drop", Group = group1, State = state2, Resources = new ObservableCollection<KanbanResource> { resource2 } },
+                new KanbanItem { Content = "Drag and drop", Group = group1, State = state3, Resources = new ObservableCollection<KanbanResource> { resource2 }, Categories = new ObservableCollection<KanbanCategory> { category1 } },
                 new KanbanItem { Content = "Preparations", Group = group2, State = state5, Resources = new ObservableCollection<KanbanResource> { resource1 } },
                 new KanbanItem { Content = "Colors", Group = group2, State = state5, Resources = new ObservableCollection<KanbanResource> { resource3 } },
                 new KanbanItem { Content = "Logo", Group = group2, State = state2, Resources = new ObservableCollection<KanbanResource> { resource3 } },
-                new KanbanItem { Content = "Sample apps", Group = group2, State = state3, Resources = new ObservableCollection<KanbanResource> { resource4 } },
+                new KanbanItem { Content = "Sample apps", Group = group2, State = state3, Resources = new ObservableCollection<KanbanResource> { resource4 }, Categories = new ObservableCollection<KanbanCategory> { category2 } },
                 new KanbanItem { Content = "Screenshots", Group = group2, State = state2, Resources = new ObservableCollection<KanbanResource> { resource4 } },
                 new KanbanItem { Content = "Web site", Group = group2, State = state1, Resources = new ObservableCollection<KanbanResource> { resource2, resource4 }, ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardSample;component/Images/WebSite.png", UriKind.RelativeOrAbsolute)) },
                 new KanbanItem { Content = "Social networking", Group = group2, State = state1, Resources = new ObservableCollection<KanbanResource> { resource1, resource3 } },
@@ -73,6 +78,7 @@ namespace KanbanBoardSample
             KanbanBoard.States = states;
             KanbanBoard.Groups = groups;
             KanbanBoard.Items = items;
+            KanbanBoard.AvailableCategories = categories;
             KanbanBoard.AvailableResources = resources;
             KanbanBoard.GroupStates = groupStates;
 
@@ -82,22 +88,26 @@ namespace KanbanBoardSample
                 Console.WriteLine("A new item was created.");
             };
 
-            KanbanBoard.UsePopupMenus = false;
-            KanbanBoard.EditItemButtonContent = "×";
-            KanbanBoard.EditItemButtonToolTipContent = "Delete item";
-            KanbanBoard.EditingItem += (sender, e) =>
-            {
-                var item = e.Item;
-                if (MessageBox.Show("Are you sure you want to delete item " + item.Content + "?", "Kanban", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
-                {
-                    items.Remove(item);
-                    Console.WriteLine("Item " + item.Content + " was deleted.");
-                }
-            };
-            KanbanBoard.EditingGroup += (sender, e) =>
-            {
-                MessageBox.Show("Editing group " + e.Group.Content + "...");
-            };
+            // Optionally, define a custom "edit" button and behavior:
+            // KanbanBoard.UsePopupMenus = false;
+            // KanbanBoard.EditItemButtonContent = "×";
+            // KanbanBoard.EditItemButtonToolTipContent = "Delete item";
+            // KanbanBoard.EditingItem += (sender, e) =>
+            // {
+            //     var item = e.Item;
+            //     if (MessageBox.Show("Are you sure you want to delete item " + item.Content + "?", "Kanban", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            //     {
+            //         items.Remove(item);
+            //         Console.WriteLine("Item " + item.Content + " was deleted.");
+            //     }
+            // };
+            // KanbanBoard.EditingGroup += (sender, e) =>
+            // {
+            //     MessageBox.Show("Editing group " + e.Group.Content + "...");
+            // };
+
+            // Otherwise, you may hide edit menu items instead:
+            KanbanBoard.AreEditMenuItemsHidden = true;
 
             KanbanBoard.ItemStateChanged += (sender, e) =>
             {
