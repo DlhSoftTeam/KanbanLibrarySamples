bedab135feacc055597296b4d3f68b2978aee414
DlhSoft <Contact@DlhSoft.com>
2024-10-15 10:10:09 +0300

    KanbanSortingByPriority Sample, Update KanbanLibrary

---
+++ KanbanBoardCustomColumns/IsEnabledKanbanStatesEditorConverter.cs
@@ -0,0 +1,26 @@
+﻿using KanbanLibrary;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KanbanBoardCustomColumnsSample.Converters
+{
+    public class IsEnabledKanbanStatesEditorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            KanbanState kanbanState = (KanbanState)values[0];
+            DataGrid KanbanStatesDataGrid = (DataGrid)values[1];
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+          
+            return KanbanStatesDataGrid.SelectedItems.Count > 0 && kanbanState != mainWindow.NewState && kanbanState != mainWindow.InProgressState && kanbanState != mainWindow.DoneState;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
