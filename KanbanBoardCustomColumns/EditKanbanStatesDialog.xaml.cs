81ee76c390fa38dc1dfc9d71f6218c3fdd65480f
DlhSoft <Contact@DlhSoft.com>
2024-10-19 13:01:47 +0300

    Edit columns, filtering (search in content), sorting samples

---
+++ KanbanBoardCustomColumns/EditKanbanStatesDialog.xaml.cs
@@ -0,0 +1,129 @@
+﻿using KanbanLibrary;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace KanbanBoardCustomColumnsSample
+{
+    /// <summary>
+    /// Interaction logic for EditKanbanStatesDialog.xaml
+    /// </summary>
+    public partial class EditKanbanStatesDialog : Window
+    {
+        public EditKanbanStatesDialog()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow MainWindow => Owner as MainWindow;
+
+        public ObservableCollection<KanbanState> KanbanStates => MainWindow.KanbanStates;
+        
+        private void KanbanState_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var KanbanState = sender as KanbanState;
+            if (KanbanState != null)
+            {
+                if (e.PropertyName == "Content")
+                {
+                    var KanbanStateDuplicate = CheckStateUnicity(KanbanState);
+                    if (KanbanStateDuplicate != null)
+                    {
+                        MessageBox.Show("Your column names aren't unique, please check the names.");
+                        KanbanState.Content = GetUniqueStateName(KanbanState.Content.ToString());
+                    }
+                }
+            }
+        }
+
+        private KanbanState CheckStateUnicity(KanbanState kanbanState)
+        {
+            if (kanbanState == null)
+                return null;
+
+            string name = kanbanState.Content.ToString();
+            var count = KanbanStates.Count(i => i.Content.ToString() == name && i != kanbanState);
+            if (count >= 1)
+            {
+                return kanbanState;
+            }
+            return null;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var ks in KanbanStates)
+                ks.PropertyChanged -= KanbanState_PropertyChanged;
+            Close();
+        }
+
+        private void AddStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            KanbanState newState = new KanbanState() { Content = "New Column" };
+            newState.Content = GetUniqueStateName(newState.Content.ToString());
+            KanbanStates.Insert( KanbanStates.Count - 1, newState);
+            newState.PropertyChanged += KanbanState_PropertyChanged;
+            Dispatcher.BeginInvoke((Action)delegate
+            {
+                KanbanStatesDataGrid.SelectedItem = newState;
+                KanbanStatesDataGrid.CurrentColumn = KanbanStatesDataGrid.Columns.First();
+                KanbanStatesDataGrid.CurrentItem = newState;
+                KanbanStatesDataGrid.BeginEdit();
+            }, System.Windows.Threading.DispatcherPriority.Background);
+        }
+
+        private string GetUniqueStateName(string proposedName)
+        {
+            while (KanbanStates.Any(s => s.Content.ToString().ToLowerInvariant() == proposedName.ToLowerInvariant()))
+            {
+                int indexOfLastSpace = proposedName.LastIndexOf(' ');
+                int lastNumber = 0;
+                if (indexOfLastSpace >= 0)
+                {
+                    if (int.TryParse(proposedName.Substring(indexOfLastSpace + 1), out lastNumber))
+                        proposedName = proposedName.Substring(0, indexOfLastSpace);
+                }
+                proposedName = proposedName + " " + (lastNumber + 1);
+            }
+            return proposedName;
+        }
+
+        private void MoveUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            int index = KanbanStatesDataGrid.SelectedIndex;
+            if (index < 1)
+                return;
+            KanbanStates.Move(index, index - 1);
+        }
+
+        private void MoveDownButton_Click(object sender, RoutedEventArgs e)
+        {
+            int index = KanbanStatesDataGrid.SelectedIndex;
+            if (index == KanbanStatesDataGrid.Items.Count - 1)
+                return;
+            KanbanStates.Move(index, index + 1);
+        }
+
+        private void DeleteStateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete the selected column?", "Question", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+                return;
+            var ks = (KanbanState)KanbanStatesDataGrid.SelectedItem;
+            ks.PropertyChanged -= KanbanState_PropertyChanged;
+            KanbanStates.Remove(ks);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (KanbanState kanbanState in KanbanStates)
+                kanbanState.PropertyChanged += KanbanState_PropertyChanged;
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            foreach (KanbanState kanbanState in KanbanStates)
+                kanbanState.PropertyChanged -= KanbanState_PropertyChanged;
+        }
+    }
+}
---
+++ KanbanBoardCustomColumns/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using KanbanLibrary;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace KanbanBoardCustomColumnsSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public KanbanState NewState, InProgressState, DoneState;
+        public ObservableCollection<KanbanState> KanbanStates => KanbanBoard.States;
+
+        private ObservableCollection<KanbanItem> KanbanItems = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NewState = new KanbanState { Content = "New" };
+            InProgressState = new KanbanState { Content = "In progress" };
+            DoneState = new KanbanState { Content = "Done" };
+            KanbanBoard.States = new ObservableCollection<KanbanState>()
+            {
+                NewState, InProgressState, DoneState
+            };
+
+            var backgroundPriority = new SolidColorBrush();
+            backgroundPriority.Color = Color.FromRgb(238, 238, 238);
+
+            var priorities = KanbanBoard.DefaultPriorities;
+
+            var lowPriority = priorities.Single(p => p.Number == 10);
+            lowPriority.Background = backgroundPriority;
+            lowPriority.Foreground = Brushes.Green;
+
+            var mediumPriority = priorities.Single(p => p.Number == 100);
+            mediumPriority.Background = backgroundPriority;
+            mediumPriority.Foreground = Brushes.CornflowerBlue;
+
+            var highPriority = priorities.Single(p => p.Number == 1000);
+            highPriority.Background = backgroundPriority;
+            highPriority.Foreground = Brushes.DarkRed;
+
+            var ClarissaResource = new KanbanResource { Content = "Clarissa Candelaria", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardCustomColumnsSample;component/Images/Clarissa.png", UriKind.RelativeOrAbsolute)),  };
+            var TysonResource = new KanbanResource { Content = "Tyson Lamberson", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardCustomColumnsSample;component/Images/Tyson.png", UriKind.RelativeOrAbsolute)) };
+            var JoannaResource = new KanbanResource { Content = "Joanna Mcamis", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardCustomColumnsSample;component/Images/Joanna.png", UriKind.RelativeOrAbsolute)) };
+            var JedResource = new KanbanResource { Content = "Jed Markovitz", ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardCustomColumnsSample;component/Images/Jed.png", UriKind.RelativeOrAbsolute)) };
+            var resources = new ObservableCollection<KanbanResource> { ClarissaResource, TysonResource, JoannaResource, JedResource };
+
+            var developmentGroup = new KanbanGroup { Content = "Development", State = InProgressState, Date = DateTime.Today, DueDate = DateTime.Today.AddDays(14), HandleBrush = Brushes.LightSlateGray,  Resources = new ObservableCollection<KanbanResource> { ClarissaResource, TysonResource } };
+            var marketingGroup = new KanbanGroup { Content = "Marketing", State = NewState, HandleBrush = Brushes.DarkRed, Resources = new ObservableCollection<KanbanResource> { JoannaResource }, ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardCustomColumnsSample;component/Images/alert.png", UriKind.RelativeOrAbsolute)) };
+            var groups = new ObservableCollection<KanbanGroup> { developmentGroup, marketingGroup };
+
+            KanbanBoard.TaskItemType.HandleBrush = Brushes.LightBlue;
+
+            KanbanItems = new ObservableCollection<KanbanItem>
+            {
+                new KanbanItem { Content = "Architecture", State = DoneState, Progress = 1.0, Priority = lowPriority, Resources = new ObservableCollection<KanbanResource> { TysonResource }, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardCustomColumnsSample;component/Images/Architecture.png", UriKind.RelativeOrAbsolute)) },
+                new KanbanItem { Content = "Date-times", State = InProgressState, Progress = 0.50, Resources = new ObservableCollection<KanbanResource> { JoannaResource }, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardCustomColumnsSample;component/Images/DateTimes.png", UriKind.RelativeOrAbsolute)) },
+                new KanbanItem { Content = "Schedules", State = InProgressState, Progress = 0.30, Priority = mediumPriority, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { JoannaResource, JedResource } },
+                new KanbanItem { Content = "Weekend issue", State = InProgressState, Progress = 0.70, Priority = highPriority, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { JedResource }, ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardCustomColumnsSample;component/Images/flag.png", UriKind.RelativeOrAbsolute)) },
+                new KanbanItem { Content = "Diagram", State = InProgressState, Progress = 0.20, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { ClarissaResource } },
+                new KanbanItem { Content = "Bars", State = NewState, Progress = 0.40, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { ClarissaResource } },
+                new KanbanItem { Content = "Nonworking time highlighting", State = DoneState, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { JedResource } },
+                new KanbanItem { Content = "Background", State = DoneState, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")) },
+                new KanbanItem { Content = "Milestone bug", State = InProgressState, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { ClarissaResource } },
+                new KanbanItem { Content = "Drag and drop", State = InProgressState, Priority = highPriority, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { TysonResource } },
+                new KanbanItem { Content = "Preparations", State = DoneState, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { ClarissaResource } },
+                new KanbanItem { Content = "Colors", Group = marketingGroup, State = DoneState, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { JoannaResource } },
+                new KanbanItem { Content = "Logo", Group = marketingGroup, State = InProgressState, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { JoannaResource } },
+                new KanbanItem { Content = "Sample apps", Group = marketingGroup, State = InProgressState, Progress = 0.20, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { JedResource } },
+                new KanbanItem { Content = "Screenshots", Group = marketingGroup, State = InProgressState, Progress = 0.40, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { JedResource } },
+                new KanbanItem { Content = "Web site", Group = marketingGroup, State = NewState, Progress = 0.50, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { TysonResource, JedResource }, ImageSource = new BitmapImage(new Uri("pack://application:,,,/KanbanBoardCustomColumnsSample;component/Images/WebSite.png", UriKind.RelativeOrAbsolute)) },
+                new KanbanItem { Content = "Social networking", Group = marketingGroup, State = NewState, Date = DateTime.Today.Add(TimeSpan.Parse("08:00:00")), DueDate = DateTime.Today.Add(TimeSpan.Parse("16:00:00")), Resources = new ObservableCollection<KanbanResource> { ClarissaResource, JoannaResource } }
+            };
+            KanbanBoard.Items = KanbanItems;
+
+            KanbanBoard.AvailableResources = resources;
+            KanbanBoard.Groups = groups;
+            KanbanBoard.States = KanbanStates;
+
+            KanbanBoard.ItemProgressVisibility = Visibility.Visible;
+            KanbanBoard.ItemProgressBrush = Brushes.CornflowerBlue;
+            KanbanBoard.ItemProgressHeight = 4.0;
+            KanbanBoard.ItemProgressLabelVisibility = Visibility.Visible;
+
+            KanbanBoard.ItemDateVisibility = Visibility.Visible;
+            KanbanBoard.ItemDueDateVisibility = Visibility.Visible;
+
+            KanbanBoard.PriorityVisibility = Visibility.Visible;
+            KanbanBoard.PriorityLabelVisibility = Visibility.Visible;
+
+            KanbanBoard.AreEditMenuItemsHidden = true;
+            KanbanBoard.AreStateExpandersVisible = true;
+        }
+
+        private void EditColumnsButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditKanbanStatesDialog editKanbanStatesDialog = new EditKanbanStatesDialog { Owner = Application.Current.MainWindow, DataContext = this };
+            editKanbanStatesDialog.ShowDialog();
+        }
+
+        private void SearchInContent()
+        {
+            var textForSearching = SearchTextBox.Text.Trim();
+            if (string.IsNullOrEmpty(textForSearching))
+            {
+                KanbanBoard.Items = KanbanItems;
+                return;
+            }
+            KanbanBoard.Items = new ObservableCollection<KanbanItem>(KanbanItems.Where(i => i.Content.ToString().Contains(textForSearching)));
+        }
+
+        private void SearchTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                SearchInContent();
+            }
+        }
+
+        private void SearchContentButton_Click(object sender, RoutedEventArgs e)
+        {
+            SearchInContent();
+        }
+    }
+}
